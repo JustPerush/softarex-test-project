02c8c30e11289c7c88970a2482d0dc83ef75f81f
Vladisalv <v.perush@mail.com>
2022-06-22 18:33:25 +0300

    added dynamic pagination

--- src/actions/useGetRequest.jsx
+++ src/actions/useGetRequest.jsx
@@ -11,11 +11,31 @@ export function useGetRequest(BASE_URL, CURATED = "", PER_PAGE = "", API_KEY) {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const scrollHandler = (event) => {
+    if (
+      event.target.documentElement.scrollHeight -
+        (event.target.documentElement.scrollTop + window.innerHeight) <
+        100 &&
+      items.length < totalCount
+    ) {
+      setIsLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  });
 
   useEffect(() => {
     setErrorMessage(null);
     setIsLoading(true);
-    fetch(BASE_URL + CURATED + PER_PAGE, {
+    fetch(BASE_URL + CURATED + `page=${currentPage}&` + PER_PAGE, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -27,16 +47,20 @@ export function useGetRequest(BASE_URL, CURATED = "", PER_PAGE = "", API_KEY) {
           throw Error(response.statusText);
         }
         setIsLoading(true);
+        setTotalCount(response.headers["x-total-count"]);
         return response.json();
       })
       .then((data) => {
-        setItems(data.photos);
+        setItems([...items, data.photos]);
+        setCurrentPage((prevState) => prevState + 1);
       })
       .catch((error) => setErrorMessage(error.message))
-      .finally(() => setIsLoading(false));
+      .finally(() => {
+        setIsLoading(false);
+      });
     return () => {
       console.log("Nothing found...");
     };
-  }, []);
+  }, [isLoading]);
   return [isLoading, errorMessage, items];
 }
